1bf7d0bb4c8f0def5b50857d7aa4c7f6fa1d54c6
Piotr Danisz <piotrdanisz123@gmail.com>
2020-10-30 15:56:57 +0100

    add icon fille

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js'
 import {settings} from '../../data/dataStore.js';
+import Icon from './Icon.js';
 
 class Column extends React.Component {
   state = {
@@ -11,26 +12,27 @@ class Column extends React.Component {
   }
     static propTypes = {
       title: PropTypes.string,
+      icon: PropTypes.string,
     }
-    addCard(title) {
+    addCard(title){
       this.setState(state => (
         {
           cards: [
             ...state.cards,
             {
-              key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+              key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
               title,
               icon: 'list-alt',
-              cards: []
+              cards:[]
             }
           ]
         }
-      ));
-    }
+      ));}
     render() {
       return (
         <section className={styles.component}>
-            <h3 className={styles.title}>{this.props.title}</h3>
+            <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon}/>
+              </span>{this.props.title}</h3>
             {this.state.cards.map(({key, ...cardProps}) => (
               <Card key={key} {...cardProps} />
             ))}
---
+++ src/components/Column/Icon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {settings} from '../../data/dataStore.js'
+
+const Icon = props => (
+
+<i className={`fas fa-${props.name}`}></i>
+
+);
+
+
+export default Icon;
