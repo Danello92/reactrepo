50784035538b45bc8cd144b6d4a5f3c23ba7ab46
Piotr Danisz <piotrdanisz123@gmail.com>
2020-11-04 22:21:46 +0100

    add cardsRedux.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
-
 import Column from '../Column/ColumnContainer.js';
 import PropTypes, {string} from 'prop-types';
-
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   // state = {
@@ -18,6 +16,7 @@ class List extends React.Component {
     description: string,
     image: PropTypes.string.isRequired,
     columns: PropTypes.array.isRequired,
+    addColumn: PropTypes.func,
   }
   static defaultProps = {
     description: settings.defaultColumnIcon,
@@ -38,7 +37,7 @@ class List extends React.Component {
   //   ));
   // }  
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} imageProp={image}/>
@@ -50,9 +49,9 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/* <div className={styles.creator}>
+        <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div> */}
+        </div>
       </section>
     );
   }
